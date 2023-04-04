2126ff595549ec3ffc4981eccc3274f204f88be3
mrcocoball <gunmaru93@gmail.com>
2023-04-04 23:19:55 +0900

    0404 - 요소 바인딩

---
+++ binding-0404/src/routes/+page.svelte
@@ -0,0 +1,106 @@
+<script>
+	let text = '';
+	let number = 3
+	let checked = false
+	let fruits = ['Apple', 'Banana', 'Cherry']
+	let selectedFruits = []
+	let group = 'Banana'
+	let textarea = ''
+	let select = 'Banana'
+	let multipleSelect = ['Banana', 'Cherry']
+
+</script>
+
+<!-- let text ==''; -->
+<section>
+	<h2>Text</h2>
+	<input type="text" bind:value={text} />
+</section>
+
+<!-- let number = 3; -->
+<section>
+	<h2>Number/Range</h2>
+	<div>
+		<input type="number" bind:value={number} min=0 max=10 />
+	</div>
+	<div>
+		<input type="range" bind:value={number} min=0 max=10 />
+	</div>
+</section>
+
+<!-- let checked = false -->
+<section>
+	<h2>Checkbox</h2>
+	<input type="checkbox" bind:checked={checked} /> Agree?
+	<label>
+		<input type="checkbox" bind:checked={checked} /> Agree?(label wrapping)
+	</label>
+</section>
+
+<!-- let fruits = ['Apple', 'Banana', 'Cherry'] 선택지-->
+<!-- let selectedFruits 담을것-->
+<section>
+	<h2>Checkbox 다중 선택</h2>
+	<strong>Selected: {selectedFruits}</strong>
+	{#each fruits as fruit}
+		<label>
+			<input type="checkbox" value={fruit} bind:group={selectedFruits} /> <!--fruit는 단방향, group 중요-->
+			{fruit}
+		</label>
+	{/each}
+</section>
+
+<!-- let group = 'Banana' -->
+<section>
+	<h2>Radio</h2>
+	<!--
+	<input type="radio" value="Apple" name="my radio" />
+	<input type="radio" value="Banana" name="my radio" />
+	<input type="radio" value="Cherry" name="my radio" />
+	-->
+	<strong>Selected : {group}</strong>
+	<label>
+		<input type="radio" value="Apple" bind:group={group} /> Apple
+	</label>
+	<label>
+		<input type="radio" value="Banana" bind:group={group} /> Banana
+	</label>
+	<label>
+		<input type="radio" value="Cherry" bind:group={group} /> Cherry
+	</label>
+</section>
+
+<!-- let textarea = '' -->
+<section>
+	<h2>Textarea</h2>
+	<pre>{textarea}</pre>
+	<textarea bind:value={textarea} />
+</section>
+
+<!-- let select = 'Banana' -->
+<section>
+	<h2>Select 단일 선택</h2>
+	<strong>Selected : {select}</strong>
+	<div>
+		<select bind:value={select}>
+			<option disabled value="">하나를 꼭 고르삼</option>
+			<option>Apple</option>
+			<option>Banana</option>
+			<option>Cherry</option>
+		</select>
+	</div>
+</section>
+
+<!-- let multipleselect = ['Banana', "Cherry"] -->
+<section>
+	<h2>Select 다중 선택(Multiple)</h2>
+	<strong>Selected : {multipleSelect}</strong>
+	<div>
+		<select multiple bind:value={multipleSelect}>
+			<option disabled value="">하나를 꼭 고르삼</option>
+			<option>Apple</option>
+			<option>Banana</option>
+			<option>Cherry</option>
+		</select>
+	</div>
+</section>
