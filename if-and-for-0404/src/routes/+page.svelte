19b3ebf6990efa98bdd819a8b8f3df5854a4503c
mrcocoball <gunmaru93@gmail.com>
2023-04-04 23:50:44 +0900

    0404 - 조건과 반복과 키 블록

--- if-and-for-0404/src/routes/+page.svelte
+++ if-and-for-0404/src/routes/+page.svelte
@@ -1,59 +1,86 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	let fruits = [
+		{id: '1', name: 'Apple'},
+		{id: '2', name: 'Banana'},
+		{id: '3', name: 'Cherry'},
+		{id: '4', name: 'Orange'},
+		{id: '5', name: 'Apple'}
+	]
+	let todos = []
+	let fruits2D = [
+		[1, 'Apple'],
+		[2, 'Banana'],
+		[3, 'Cherry'],
+		[4, 'Orange']
+	]
+	let user = {
+		name: 'cocoball',
+		age: '30',
+		email: 'test@mail.com'
+	}
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h2>기본</h2>
+	<!-- {#each 배열 as 속성} {/each} -->
+	{#each fruits as fruit}
+		<div>{fruit.name}</div>
+	{/each}
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+<section>
+	<h2>순서(index)</h2>
+	<!-- {#each 배열 as 속성, 순서} {/each} -->
+	{#each fruits as fruit, index}
+		<div>{index} / {fruit.name}</div>
+	{/each}
+</section>
 
-	h1 {
-		width: 100%;
-	}
+<section>
+	<h2>아이템 고유화(key)</h2>
+	<!-- {#each 배열 as 속성, 순서 (키)} {/each} -->
+	{#each fruits as fruit, index (fruit.id)}
+		<div>{index} / {fruit.name}</div>
+	{/each}
+</section>
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+<section>
+	<h2>빈 배열 처리(else)</h2>
+	<!-- {#each} {:else} {/each} -->
+	{#each todos as todo (todo.id)}
+		<div>{todo.name}</div>
+	{:else}
+		<div>아이템 읎다</div>
+	{/each}
+</section>
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<section>
+	<h2>구조 분해(destructuring)</h2>
+	<!-- {#each 배열 as {id, name}} {/each} -->
+	{#each fruits as {id, name} (id)}
+		<div>{name}</div>
+	{/each}
+</section>
+
+<section>
+	<h2>2차원 배열</h2>
+	<!-- {#each 배열 as [id, name]} {/each} -->
+	{#each fruits as [id, name] (id)}
+		<div>{name}</div>
+	{/each}
+</section>
+
+<section>
+	<h2>나머지 연산자(rest)</h2>
+	<!-- {#each 배열 as {id, ...rest}} {/each} -->
+	{#each fruits as {id, ...rest} (id)}
+		<div>{rest.name}</div>
+	{/each}
+</section>
+
+<section>
+	<h2>객체 데이터</h2>
+	{#each Object.entries(user) as [key, value] (key)} <!-- JS 문법 -->
+		<div>{key} : {value}</div>
+	{/each}
+</section>
